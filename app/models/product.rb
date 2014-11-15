4f83cdf9ccf73fd26d7f187c618c7508aa0cd81e
Dung Nguyen <dungntnew@gmail.com>
2014-11-16 01:23:05 +0900

    add validate min-max length of title

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,16 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
  
   # ensure each product has a unique title
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true 
+  
+  # ensuare title has length with mininum: 2 and maximum: 20
+  validates :title, length: {
+    minimum: 5,
+    maximum: 20,
+    too_long: 'title too long, must less than 20',
+    too_short: 'title too short, must longger 5'
+  
+  }
   
   # validate that the url entered for the image is valid.
   validates :image_url, allow_blank:true, format: {
