fd4c8f17e0b40d54ae5cdbc88d7d6b546ca1c255
Dung Nguyen <dungntnew@gmail.com>
2014-11-18 22:21:37 +0900

    add mailer for test

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
-  PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
+  PAYMENT_TYPES = ["Check", "Credit card", "Paypal"]
   has_many :line_items, dependent: :destroy
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: PaymentType.names
+  validates :pay_type, inclusion: PAYMENT_TYPES
  
   def add_line_items_from_cart(cart) 
     cart.line_items.each do |item|
