a45a9c557ef38f5c31952e0b7c4f44e7d3ef6bcb
Dung Nguyen <dungntnew@gmail.com>
2014-11-18 21:46:04 +0900

    fix play_type -> pay_type

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -32,7 +32,7 @@ class OrdersControllerTest < ActionController::TestCase
 
   test "should create order" do
     assert_difference('Order.count') do
-      post :create, order: { address: @order.address, email: @order.email, name: @order.name, play_type: @order.play_type }
+      post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
     assert_redirected_to store_path
@@ -49,7 +49,7 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should update order" do
-    patch :update, id: @order, order: { address: @order.address, email: @order.email, name: @order.name, play_type: @order.play_type }
+    patch :update, id: @order, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     assert_redirected_to order_path(assigns(:order))
   end
 
